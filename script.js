9ae2e5825641855eee3cb817d3ef766ef383c309
gtrman97 <andrew.killian@g.austincc.edu>
2021-07-31 13:53:25 -0500

    solved reset function problem

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let moves = 0; 
 
 function move(id) {
+    
     if (moves % 2 == 0) {
         document.getElementById(id).innerHTML = 'X'; 
         moves++; 
@@ -9,8 +10,12 @@ function move(id) {
         document.getElementById(id).innerHTML = 'O'; 
         moves++; 
     }
+
 }
 function reset() {
-    document.getElementsByClassName("square").innerHTML = "";
-    console.log(5);
+
+    for (let i=0; i<9; i++){
+        document.getElementsByClassName("square")[i].innerHTML = " "; 
+    }
+    
 }
