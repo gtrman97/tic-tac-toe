4fc0d442dfc70bd9ea445eb4293fb0f7739dd158
gtrman97 <andrew.killian@g.austincc.edu>
2021-07-31 02:28:55 -0500

    linked function and button

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let moves = 1; 
 
-function move() {
+function move(id) {
     if (moves % 2 == 0) {
-
+        document.getElementById(id).innerHTML = 'X'; 
     }
     else {
-        
+        document.getElementById(id).innerHTML = 'O'; 
     }
 }
