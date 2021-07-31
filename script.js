7451b79cfcd9d4fbd35badf95ac3faf2353ba5b6
gtrman97 <andrew.killian@g.austincc.edu>
2021-07-31 02:35:18 -0500

    fixed move function

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-let moves = 1; 
+let moves = 0; 
 
 function move(id) {
+    console.log(id);
+    console.log(3);
     if (moves % 2 == 0) {
         document.getElementById(id).innerHTML = 'X'; 
+        moves++; 
     }
     else {
         document.getElementById(id).innerHTML = 'O'; 
+        moves++; 
     }
 }
